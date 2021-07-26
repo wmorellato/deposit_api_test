df544c6861211a6ed87fd960c21e1de79c108807
Weslley Morellato <wbueno@ebi.ac.uk>
2021-07-26 15:57:17 -0300

    Create deposition

--- src/deposit_api/deposit.py
+++ src/deposit_api/deposit.py
@@ -1,8 +1,11 @@
 import os
+import requests
 import webbrowser
+from pathlib import Path
 
-KEY_FILE = '~/.onedep.jwt'
+KEY_FILE = os.path.join(Path.home(), '.onedep_api.jwt')
 AUTH_URL = 'http://pdbe-worker-1.ebi.ac.uk:12000/deposition/auth/authenticate'
+NEW_DEP_URL = 'http://pdbe-worker-1.ebi.ac.uk:12000/deposition/api/new'
 
 class Deposit:
     def __init__(self):
@@ -10,6 +13,7 @@ class Deposit:
     
     def new(self):
         self.authenticate()
+        self.create_deposition()
     
     def authenticate(self):
         if os.access(KEY_FILE, os.R_OK):
@@ -20,12 +24,24 @@ class Deposit:
         
         webbrowser.open_new_tab(AUTH_URL)
 
-        print('If now browser shows up, copy this link and open it in a browser: %s' % AUTH_URL)
+        print('If a browser does not open, copy this link and open it in a working browser: %s' % AUTH_URL)
         
         try:
             self._api_key = input('Enter your API key: ')
-            return True
-        except:
-            print('Error getting an API key')
+            
+            with open(KEY_FILE, 'w', encoding='utf-8') as fp:
+                fp.write(self._api_key)
+        except Exception as e:
+            print('Error getting an API key, %s', e)
         
         return False
+    
+    def create_deposition(self):
+        r = requests.post(NEW_DEP_URL, headers={
+            'Authorization': 'Bearer %s' % self._api_key
+        }, data={
+            'location': 'United Kingdom',
+            'email': 'w3_pdb05@localhost',
+            'password': 'notNeededAnymore',
+            'experiments': [{'method': 'xray'}]
+        })
